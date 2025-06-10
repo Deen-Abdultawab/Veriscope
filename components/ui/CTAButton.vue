85c4dc0b4727f7fc764e88edfef20059cc63401c
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2025-06-10 22:34:18 +0100

    contact button linked to whatsapp

--- components/ui/CTAButton.vue
+++ components/ui/CTAButton.vue
@@ -1,11 +1,12 @@
 <template>
-    <nuxt-link 
-    to="/Contact"
+    <a 
+    href="https://wa.me/2349137978816"
+    target="_blank"
     class="btn mt-[1.5rem] md:mt-[2rem] w-full md:w-fit hover:bg-[#0d3a7a] transition-colors transitionEffect text-center"
     :style="{'background-color': `${bG}`, 'color': `${textCol}`}"
     >
     Contact Us
-   </nuxt-link>
+   </a>
 </template>
 
 <script setup>
